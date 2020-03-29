cf36c914a8d21c09754fd378451792a1cd948a52
Yves-Holvoet <yves.holvoet@gmail.com>
2020-03-29 16:02:39 +0200

    Delete __init__.py

--- PingPong/Python/Test/__init__.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-# create file handler for logging
-logging.basicConfig(filename='test.python.log', 
-    format='%(asctime)s %(levelname)-8s %(message)s', 
-    level=logging.INFO, 
-    datefmt='%Y-%m-%d %H:%M:%S')
